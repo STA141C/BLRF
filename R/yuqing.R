ae561582fe07670dfb7bc1a75d77bc0f1fe3fd03
yuqing-yang <46581451+yuqing-yang@users.noreply.github.com>
2020-03-09 11:44:42 -0700

    2

--- R/yuqing.R
+++ R/yuqing.R
@@ -8,14 +8,14 @@ blrf <- function(formula, data, newdata, s, gamma, r, workers = 1){
   future_map(seq_len(s),
 
              ~{
-                I <- sample(n, b, replace = FALSE)
+                index <- sample(n, b, replace = FALSE)
 
 
                 map_dbl(seq_len(r),
 
                     ~{
                       freqs <- rmultinom(1, n, rep(1, b))
-                      t <- tree(formula, I, weights = freqs)
+                      t <- tree(formula, data[index,], weights = freqs)
                       predict(t, newdata)
                     }
                     ) %>%
